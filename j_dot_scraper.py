41b1a8eb60ad03ede2fabfc878618dfa0cd65fdc
Khawaja Osama <khawajaosama007@gmail.com>
2019-04-22 00:13:55 +0500

    fourth commit

--- j_dot_scraper.py
+++ j_dot_scraper.py
@@ -1,13 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
+page=0
 data={'product name':[],'image url':[],'product category':[]}
-for page in range(1,8):
+while(True):
+    page += 1
     response =  requests.get(f'https://www.junaidjamshed.com/mens.html?p={page}')
-    links=[]
     soup = BeautifulSoup(response.text,'html.parser')
-    
     
     for product in soup.find_all('a', {'class':'product-item-link'}):
         data['product name'].append(product.text.strip())
@@ -21,5 +20,11 @@ for page in range(1,8):
         category = soup2.find_all('td',{'class':'col data'})
         if not category: data['product category'].append(None)
         else: data['product category'].append(category[-1].text)
+    print(str(page)+' Done')
+    if not soup.find_all('a',{'class': 'action next'}):
+        break
 data = pd.DataFrame.from_dict(data)
+
+#data=data.drop_duplicates(subset='product name', keep='first')
+#data=data.dropna(axis=0,how='all')
 data.to_csv('data_jdot.csv')
