1df3c99203f83327b0450cb07018728e6c7af649
Khawaja Osama <khawajaosama007@gmail.com>
2019-04-22 14:38:12 +0500

    jdot scraper updated

--- j_dot_scraper.py
+++ j_dot_scraper.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 page=0
-data={'product name':[],'image url':[],'product category':[]}
+data={'product name':[],'image url':[],'product category':[],'price':[]}
 while(True):
     page += 1
     response =  requests.get(f'https://www.junaidjamshed.com/mens.html?p={page}')
@@ -10,19 +10,26 @@ while(True):
     
     for product in soup.find_all('a', {'class':'product-item-link'}):
         data['product name'].append(product.text.strip())
+        
     for img_url in soup.find_all('img',{'class':'photo image'}):
         data['image url'].append(img_url.get('src'))
     
     for link in soup.find_all('a',onclick=True,href=True):
-        response2 = requests.get(link.get('href'))
-        soup2 = BeautifulSoup(response2.text,'html.parser')
+        soup2 = BeautifulSoup(requests.get(link.get('href')).text,'html.parser')
+        
+        data['price'].append(soup2.find_all('span',{'class':'price'})[0].text)
         
         category = soup2.find_all('td',{'class':'col data'})
-        if not category: data['product category'].append(None)
-        else: data['product category'].append(category[-1].text)
+        if category: data['product category'].append(category[-1].text)
+        else:
+            category = soup2.find_all('div',{'class':'value','itemprop':'description'})
+            data['product category'].append(category[-1].text) if category else data['product category'].append(None)
+    
     print(str(page)+' Done')
+    
     if not soup.find_all('a',{'class': 'action next'}):
         break
+        
 data = pd.DataFrame.from_dict(data)
 
 #data=data.drop_duplicates(subset='product name', keep='first')
